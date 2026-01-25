8385dfc4e1f1d510b06bbe2f28a1190a62536b1a
rodrigs0510 <rodrigs0510@gmail.com>
2026-01-25 00:53:33 -0500

    header.py

--- layout/header.py
+++ layout/header.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOGO_PATH = os.path.join(BASE_DIR, "assets", "imagenes", "logo.png")
 
 def render_header():
-    st.image("assets/imagenes/logo.png", width=80)
+    st.image(LOGO_PATH, width=80)
