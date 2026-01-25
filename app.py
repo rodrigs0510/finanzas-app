5a414381f27a46cf2726dec6d7277c711d1ca8e3
rodrigs0510 <rodrigs0510@gmail.com>
2026-01-25 01:11:18 -0500

    app.py

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 import streamlit as st
+import base64
 from layout.header import render_header
 from layout.registro import render_registro
 from layout.dashboard import render_dashboard
 
+
 st.set_page_config(
     page_title="CAPIGASTOS",
     layout="wide",
-    page_icon="🐹"
+    page_icon="❤️"
 )
+
+def load_image_base64(path):
+    try:
+        with open(path, "rb") as f:
+            return base64.b64encode(f.read()).decode()
+    except:
+        return ""
+
+bg_fondo = load_image_base64("images/fondo.jpg")
 
 render_header()
 render_registro()
 render_dashboard()
+
 
 
 
