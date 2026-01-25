0eebcc4f66fe2e393776046a12477b2a7af4d954
rodrigs0510 <rodrigs0510@gmail.com>
2026-01-25 01:23:13 -0500

    dashboard.py

--- layout/dashboard.py
+++ layout/dashboard.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
-def render_dashboard():
-    st.markdown("## 📊 Dashboard")
-    st.info("Aquí irá el resumen y las cuentas")
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.selectbox("Mes", ["Enero", "Febrero", "Marzo"])
+
+with col2:
+    st.selectbox("Año", [2024, 2025])
+
+with col3:
+    st.metric("Saldo total", "S/ 2,500")
+
+with col4:
+    st.metric("Ahorro total", "S/ 800")
